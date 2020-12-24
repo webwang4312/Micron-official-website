0760d3788bd222e7b652048262d990a41b0a32fb
webwang4312 <63950083+webwang4312@users.noreply.github.com>
2020-12-24 14:36:06 +0800

    first commit

---
+++ 微米-mobile-12.8/README.md
@@ -0,0 +1,28 @@
+# textone
+微米官网手机端源代码
+12.14修改议事部人员3人变5人，监督部7人变3人,修改了议事同意阶段的样式,APP.VUE修改reload
+12.23分类vote，proposal文件夹
+12.24新建develop文件夹，创建developer.vue,developerdetail.vue
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
---
+++ 微米-mobile-12.8/src/views/develop/Developer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="developer">
+    <slider></slider>
+    11111111111
+    <v-footer></v-footer>
+  </div>
+</template>
+
+<script>
+import slider from "@components/common/slider";
+import vFooter from "@components/common/vFooter";
+export default {
+  name: "developer",
+  data() {
+    return {
+     
+    };
+  },
+  components: { slider, vFooter },
+  created() {
+   
+   
+  },
+
+  mounted() {},
+  destroyed() {},
+  methods: {
+   
+  }
+};
+</script>
+<style lang="less">
+.mencen {
+  background: rgba(0, 0, 0, 1);
+  opacity: 0.5;
+}
+.developer {
+  width: 100%;
+  height: auto;
+  min-height: 100vh;
+  background-color: black;
+
+ 
+}
+</style>
